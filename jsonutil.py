7121c76d22ec3cfc5a5b7250652f94f745d6925c
David Paleino <dapal@debian.org>
2010-11-05 21:11:03 +0100

    Add 'set' command

--- jsonutil.py
+++ jsonutil.py
@@ -6,17 +6,24 @@
 import cjson
 import sys
 
+global original
+
 def main():
+    global original
     json = eval(''.join(open(sys.argv[1])))
     original = json
 
     cmd = sys.argv[2]
     path = sys.argv[3]
+    value = sys.argv[4]
+    force = False
 
     if cmd == 'get':
         print get(json, path)
     elif cmd == 'typeof':
         print typeof(json, path)
+    elif cmd == 'set':
+        print set_value(json, path, value, force)
 
 def get(json, path):
     strippath = path[1:]
@@ -67,6 +74,40 @@ def typeof(json, path):
             return str(type(json[key]))
     elif type(json) in [str, int, float]:
         return str(type(json))
+
+def set_value(json, path, value, force=False):
+    strippath = path[1:]
+    newpath = None
+    if '/' in strippath:
+        key, newpath = strippath.split('/', 1)
+        newpath = '/'+newpath
+    else:
+        key = strippath
+
+    if key == '':
+        return 'Unsupported.'
+
+    if type(json) == list:
+        if newpath:
+            return set_value(json[int(key)], newpath, value)
+        else:
+            if type(json[int(key)]) in [list, dict]:
+                return 'Not setting the value, please use --force.'
+            else:
+                json[int(key)] = value
+                return original
+    if type(json) == dict:
+        if newpath:
+            return set_value(json[key], newpath, value)
+        else:
+            if type(json[key]) in [list, dict]:
+                return 'Not setting the value, please use --force.'
+            else:
+                json[key] = value
+                return original
+    elif type(json) in [str, int, float]:
+        json = value
+        return original
 
 if __name__ == '__main__':
     main()
