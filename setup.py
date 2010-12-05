c985b0de50fc1c77c69ebe54d424d05a23c1f90f
David Paleino <dapal@debian.org>
2010-12-05 17:16:59 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from setuptools import setup
+#import sys, os
+from jsonutil import version
+
+setup(name='jsonutil',
+      version=version,
+      description="JSON Manipulation Utility",
+      long_description="""\
+jsonutil is a JSON manipulation utility. It lets you extract various bits of
+data from a JSON object, with an easy-to-understand "path" syntax.
+""",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Utilities',
+      ],
+      keywords='json',
+      author='David Paleino',
+      author_email='d.paleino@gmail.com',
+      url='http://www.hanskalabs.net/projects/jsonutil',
+      license='GPL-3+',
+      packages=[],
+      scripts=['jsonutil.py'],
+      include_package_data=False,
+      zip_safe=False,
+      requires=[
+          'cjson',
+      ],
+)
